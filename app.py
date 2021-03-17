36fdfbbbc2f352fd128dfad2987109c3df144002
Carlos Cavalcanti <crl.albu@gmail.com>
2021-03-17 18:01:31 -0300

    Update API

--- app.py
+++ app.py
@@ -1,42 +1,67 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 import pymongo
+
+from db import Config
 
 import db
 
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def flask_mongodb_altas():
-    return "flask mongodb altas!"
+class Frases(Resource):
+    
 
-@app.route('/teste')
-def teste():
-    #CONNECTION_STRING = "mongodb+srv://pensedatabase:iePhei4a@cluster0-mvnvv.mongodb.net/test?retryWrites=true&w=majority"
-    #client = pymongo.MongoClient(CONNECTION_STRING)
-    #print(client.list_database_names())
-    #frase = client.pensedatabase.frases.find_one()
-    frases = db.client.pensedatabase.frases.find().limit(10)
-    #$teste = db.user_collection.find().limit(1)
-    retorno = {}
-    for frase in frases:
-        retorno = {"autor": frase['autor'], "frase": frase['frase']}
+    def get(self, name):
+        retorno = []
+        db = pymongo.MongoClient(Config.CONNECTION_STRING)
+        #client = pymongo.MongoClient(CONNECTION_STRING)
+        
+        query = {"autor": name + "/"}
+    
+        frases  = db.pensedatabase.frases.find(query).sort("_id", pymongo.DESCENDING).limit(100)
+        #frases = db.pensedatabase.frases.find().limit(10)
+
+        for frase in frases:
+            retorno.append({"frase": frase['frase']})
+
+        db.close()
+
+        return {'auto': name, 'frases': retorno}
+
+api.add_resource(Frases, '/frases/<string:name>')
+
+app.run(port=5000)
+# @app.route('/')
+# def flask_mongodb_altas():
+#     return "flask mongodb altas!"
+
+# @app.route('/teste')
+# def teste():
+    
+#     frases = db.client.pensedatabase.frases.find().limit(10)
+    
+#     retorno = {}
+#     for frase in frases:
+        
+#         retorno = {"autor": frase['autor'], "frase": frase['frase']}
             
         
-    return jsonify(retorno)
+#     return retorno
 
-@app.route('/author/<string:name>')
-def getAuthor(name):
-    retorno = []
-    query = {"autor": name + "/"}
-    print(query)
-    frases  = db.client.pensedatabase.frases.find(query).sort("_id", pymongo.DESCENDING).limit(10)
+# @app.route('/author/<string:name>')
+# def getAuthor(name):
+#     retorno = []
+#     query = {"autor": name + "/"}
+#     print(query)
+#     frases  = db.client.pensedatabase.frases.find(query).sort("_id", pymongo.DESCENDING).limit(10)
     
-    for frase in frases:
-        retorno.append({"frase": frase['frase']})
+#     for frase in frases:
+#         retorno.append({"frase": frase['frase']})
         
 
-    return jsonify(retorno)
+#     return jsonify(retorno)
 
 # @app.route('/myfrases', 'POST')
 # def favFrase():
@@ -57,5 +82,5 @@ def getAuthor(name):
 #     #return f"{retorno}"
 
 
-if __name__ == '__main__':
-    app.run(port=8000)
+# if __name__ == '__main__':
+#     app.run(port=8000)
