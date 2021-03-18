7d5435732ff12c83d29beeac53ea9b47a9c55c3b
Carlos Cavalcanti <crl.albu@gmail.com>
2021-03-18 17:19:15 -0300

    Verificação se usuário já existe

--- user.py
+++ user.py
@@ -61,6 +61,9 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
+        if User.find_by_username(data['username']):
+            return {"message": "Um usuário com esse nome já existe"}, 400
+
 
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
