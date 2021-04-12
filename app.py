6e9088e558432ed91c30c19e90007bb295f790bb
Carlos Cavalcanti <crl.albu@gmail.com>
2021-04-12 15:44:36 -0300

    Adding Heroku required files

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import  Api
 from bson import ObjectId
-from flask_jwt import JWT, jwt_required
-import pymongo
+from flask_jwt import JWT
+#import pymongo
 
 from security import authenticate, identity
 from user import UserRegister
+from frases import Frases
 
 from db import Config
 
@@ -18,25 +19,7 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  #/auth
 
-class Frases(Resource):
-    
-    @jwt_required()
-    def get(self, name):
-        retorno = []
-        db = pymongo.MongoClient(Config.CONNECTION_STRING)
-        #client = pymongo.MongoClient(CONNECTION_STRING)
-        
-        query = {"autor": name + "/"}
-    
-        frases  = db.pensedatabase.frasesnovas.find(query).sort("_id", pymongo.DESCENDING).limit(100)
-        #frases = db.pensedatabase.frases.find().limit(10)
 
-        for frase in frases:
-            retorno.append({"frase": frase['frase'], "_id": str(frase['_id'])})
-
-        db.close()
-        
-        return {'autor': name, 'frases': retorno}
 
 api.add_resource(Frases, '/frases/<string:name>')
 api.add_resource(UserRegister, '/register')
